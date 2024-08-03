8b47bb28273457c0ba6572c7b5c606c7dbdd6452
Daniel Barra <dmbarra@gmail.com>
2024-08-03 12:16:48 -0300

    chore: must to be configured lint properly

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,8 +3,8 @@ import pluginJs from "@eslint/js";
 
 export default [
   { languageOptions: { globals: globals.browser } },
-  pluginJs.configs.recommended,
+  // pluginJs.configs.recommended,
   {
-    ignores: [".config/*"],["/**/node_modules/*", ".git/"]
+    ignores: [".config/*", "node_modules/*", ".git/"],
   },
 ];
